6aeb89c4b7cfe339fe4db02ede4d2b1c960bc5de
asn0998 <amadousellendiaye@gmail.com>
2024-06-05 04:54:00 -0400

    End series matplotlib

--- Matplotlib-9-suite.py
+++ Matplotlib-9-suite.py
@@ -2,8 +2,10 @@ from matplotlib import pyplot as plt
 import random
 from itertools import count
 from matplotlib.animation import FuncAnimation
+import pandas as pd
 
-
+"""
+First example
 x_vals = []
 y_vals = []
 
@@ -14,6 +16,27 @@ def animate(i):
     
     plt.cla()
     plt.plot(x_vals, y_vals)
+    plt.tight_layout()
+    plt.grid(True)
+    
+ani = FuncAnimation(plt.gcf(),animate,1000)
+
+plt.show()
+"""
+
+
+def animate(i):
+    data= pd.read_csv('data_gen.csv')
+    x =data['x_value']
+    y1=data['total_1']
+    y2=data['total_2']
+    
+    plt.cla()
+
+    plt.plot(x, y1, label='Channel 1')
+    plt.plot(x,y2, label='channel 2')
+
+    plt.legend(loc='upper left')
     plt.tight_layout()
     plt.grid(True)
     
