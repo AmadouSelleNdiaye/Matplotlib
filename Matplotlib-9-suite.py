4855e354da7ac2440886df6315be2172139c0522
asn0998 <amadousellendiaye@gmail.com>
2024-06-04 23:01:34 -0400

    Live data

---
+++ Matplotlib-9-suite.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+import random
+from itertools import count
+from matplotlib.animation import FuncAnimation
+
+
+x_vals = []
+y_vals = []
+
+index=count()
+def animate(i):
+    x_vals.append(next(index))
+    y_vals.append(random.randint(0,5))
+    
+    plt.cla()
+    plt.plot(x_vals, y_vals)
+    plt.tight_layout()
+    plt.grid(True)
+    
+ani = FuncAnimation(plt.gcf(),animate,1000)
+
+plt.show()
+    
